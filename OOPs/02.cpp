c7a7ccf45a887d456e8cce34814d8ef321c8dfe3
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2021-11-19 19:50:08 +0530

    Create 02.cpp

---
+++ OOPs/02.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Shop
+{
+    int itemId[100];    // declared private array
+    int itemPrice[100];  // declared private array
+    int counter;        // declared private variable
+
+public:
+    void initCounter(void) { counter = 0; }
+    void setPrice(void);
+    void displayPrice(void);
+};
+
+void Shop ::setPrice(void)
+{
+    cout << "Enter Id of your item no. " << counter + 1 << endl;
+    cin >> itemId[counter];
+    cout << "Enter Price of item no. " <<itemId[counter]<< endl;
+    cin >> itemPrice[counter];
+    counter++;
+}
+
+void Shop ::displayPrice(void)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        cout << "The Price of item with Id " << itemId[i] << " is " << itemPrice[i] << endl;
+    }
+}
+
+int main()
+{
+    Shop dukaan;
+    dukaan.initCounter();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.setPrice();
+    dukaan.displayPrice();
+    return 0;
+}
